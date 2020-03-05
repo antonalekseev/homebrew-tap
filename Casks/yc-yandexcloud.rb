3541b57e84b2508974331b8cd960d9574f563b4f
Anton Alekseev <anton@alekseev.cc>
2020-03-06 00:46:02 +0300

    Update yc-yandexcloud to 0.52.1

--- Casks/yc-yandexcloud.rb
+++ Casks/yc-yandexcloud.rb
@@ -1,6 +1,6 @@
 cask 'yc-yandexcloud' do
-  version '0.51.0'
-  sha256 '52031b98a4121fd0196cdbcf1f35bf364bfafe60cd4cfb1f579560f14213ac91'
+  version '0.52.1'
+  sha256 '1d05a31914c9154e0250412d4c99804d01aac406f7001216f043b83eda141830'
 
   url "https://storage.yandexcloud.net/yandexcloud-yc/release/#{version}/darwin/amd64/yc"
   name 'Yandex Cloud CLI'
