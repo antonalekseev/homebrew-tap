9b7afcb5998e98b5413798e0c034f5ae8acf13f2
Anton Alekseev <anton@alekseev.cc>
2022-09-13 16:38:01 +0300

    hashi-up doesn't provdide binaries for Apple Silicon

--- Casks/hashi-up.rb
+++ Casks/hashi-up.rb
@@ -3,23 +3,9 @@ cask 'hashi-up' do
   name 'hashi-up'
   homepage 'https://github.com/jsiebens/hashi-up'
 
-  on_intel do
-    version "0.16.0"
-    sha256 "4487c361493362c8f19fecb80097695ab6e1303c74f290aba8f66c55d1eeb726"
-    url "https://github.com/jsiebens/hashi-up/releases/download/v#{version}/hashi-up-darwin"
-  end
+  version "0.16.0"
+  sha256 "4487c361493362c8f19fecb80097695ab6e1303c74f290aba8f66c55d1eeb726"
+  url "https://github.com/jsiebens/hashi-up/releases/download/v#{version}/hashi-up-darwin"
 
-  on_arm do
-    version "0.16.0"
-    sha256 "4487c361493362c8f19fecb80097695ab6e1303c74f290aba8f66c55d1eeb726"
-    url "https://github.com/jsiebens/hashi-up/releases/download/v#{version}/hashi-up-darwin"
-  end
-
-  on_intel do
-    binary "hashi-up-darwin", target: 'hashi-up'
-  end
-
-  on_arm do
-    binary "hashi-up-darwin", target: 'hashi-up'
-  end
+  binary "hashi-up-darwin", target: 'hashi-up'
 end
