338595d8d93406da9ba991474d0bf095c45c8ab1
Anton Alekseev <anton@alekseev.cc>
2022-12-19 19:09:46 +0700

    Refactor zls

--- Casks/zls.rb
+++ Casks/zls.rb
@@ -3,17 +3,13 @@ cask 'zls' do
   name 'zls'
   homepage 'https://github.com/zigtools/zls'
 
-  on_intel do
-    version "0.10.0"
-    sha256 "55691a2d75e7d3fae9375d8683f46be7469cbf192b4dea75200c753406cb3553"
-    url "https://github.com/zigtools/zls/releases/download/#{version}/x86_64-macos.tar.zst"
-  end
+  arch arm: "aarch64", intel: "x86_64"
+  version "0.10.0"
+  url "https://github.com/zigtools/zls/releases/download/#{version}/#{arch}-macos.tar.zst"
+  sha256 arm:  "a3526620670bf58c51787751d0eabb0e1d794b28f0939c2e2d98de1b2b4a75fe",
+         intel: "55691a2d75e7d3fae9375d8683f46be7469cbf192b4dea75200c753406cb3553"
 
-  on_arm do
-    version "0.10.0"
-    sha256 "a3526620670bf58c51787751d0eabb0e1d794b28f0939c2e2d98de1b2b4a75fe"
-    url "https://github.com/zigtools/zls/releases/download/#{version}/aarch64-macos.tar.zst"
-  end
+  depends_on formula: "zstd"
 
   binary "bin/zls", target: 'zls'
 end
