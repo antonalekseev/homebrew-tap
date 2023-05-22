36e67110e37f8b28aa1ad72317bd2350b6be22c1
Anton Alekseev <anton@alekseev.cc>
2023-05-22 11:01:27 +0700

    Update yc-yandexcloud to 0.104.0

--- Casks/yc-yandexcloud.rb
+++ Casks/yc-yandexcloud.rb
@@ -3,14 +3,14 @@ cask 'yc-yandexcloud' do
   homepage 'https://cloud.yandex.ru/docs/cli/'
 
   on_intel do
-    version "0.94.0"
-    sha256 "573bd61fd0733feb26b686e8589effbfe45ebcf7046bd1b5d939cfd0393e3fde"
+    version "0.104.0"
+    sha256 "ede45fa4e8f92757ee374a40f3fc93c485c891579b89c092680982c1f03dbf81"
     url "https://storage.yandexcloud.net/yandexcloud-yc/release/#{version}/darwin/amd64/yc"
   end
 
   on_arm do
-    version "0.94.0"
-    sha256 "573bd61fd0733feb26b686e8589effbfe45ebcf7046bd1b5d939cfd0393e3fde"
+    version "0.104.0"
+    sha256 "5c0294deb912b80a1b8fa8040d6b8cab45936930f40cdb7a5c8707c505e16b70"
     url "https://storage.yandexcloud.net/yandexcloud-yc/release/#{version}/darwin/arm64/yc"
   end
 
