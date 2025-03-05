767d6217430b26d3f09bbf3d9f1c02308493135e
Anton Alekseev <anton@alekseev.cc>
2025-03-06 05:50:17 +0700

    Upgrade zls to 0.13.0

--- Casks/zls.rb
+++ Casks/zls.rb
@@ -4,10 +4,10 @@ cask 'zls' do
   homepage 'https://github.com/zigtools/zls'
 
   arch arm: "aarch64", intel: "x86_64"
-  version "0.11.0"
-  url "https://github.com/zigtools/zls/releases/download/#{version}/zls-#{arch}-macos.tar.gz"
-  sha256 arm:  "8096da4115bfe05f174ec579812ab9580f16f0eeda4f73abbd44474ee994fd15",
-         intel: "cfcd171b0d977a22cb03a40e8913ae4890da296ed085aa0b81f76832062a66f2"
+  version "0.13.0"
+  url "https://github.com/zigtools/zls/releases/download/#{version}/zls-#{arch}-macos.tar.xz"
+  sha256 arm:  "9848514524f5e5d33997ac280b7d92388407209d4b8d4be3866dc3cf30ca6ca8",
+         intel: "4b63854d6b76810abd2563706e7d768efc7111e44dd8b371d49198e627697a13"
 
   binary "bin/zls", target: 'zls'
 end
